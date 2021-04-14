2fa9251a7dc0b2b3c243157d381d6c3b8a14ebe3
Stefi99R <stefan.radojevic@uns.ac.rs>
2021-04-14 18:06:08 +0200

    Added main route for ads

---
+++ server/services/ad.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const { Ad, User } = require('../database/models');
+const { Op, fn, col } = require('sequelize');
+
+const getAds = async (req, res, next) => {
+    try {
+        let {
+            sortBy = "createdAt",
+            asc = true,
+            onlyMe = '',
+            limit = 20,
+            category = "",
+            price,
+            search = ""
+        } = req.query;
+        
+        const where = {},
+        // for filtering
+        if (onlyMe !== '') {
+            where.userId = onlyMe;
+        }
+        if (category !== "") {
+            where.category = category;
+        }
+        if (price === 'min') {
+            sortBy = fn('min', col('price'));
+        } else if (price === 'max') {
+            sortBy = fn('max', col('price'));
+        }
+        if (search !== "") {
+            where.name = { [Op.iLike]: '%' + seacrh + '%' }
+        }
+        
+        const ads = await Ad.findAll({
+            group: ["Ad.id", "User.id"],
+            where,
+            order: [[sortBy, asc ? "ASC" : "DESC"]],
+            limit,
+            include: [{
+                model: User,
+                attributes: ['username', 'phone', 'createdAt'],
+            }]
+        });
+
+        res.json(ads);
+
+    } catch (error) {
+        return next(error);
+    }
+}
